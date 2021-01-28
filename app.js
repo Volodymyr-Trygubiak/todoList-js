95768e159edfb6f6ec96c13922982b05162269a5
Volodymyr Trygubiak <trigubiak@gmail.com>
2021-01-28 16:47:00 +0200

    added new function for todolist

--- app.js
+++ app.js
@@ -24,4 +24,92 @@ const tasks = [{
   },
 ];
 
-(function (arrOfTasks) {})(tasks);
+(function (arrOfTasks) {
+  const objOfTask = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  // Elements UI
+  const listContainer = document.querySelector('.tasks-list-section .list-group');
+  const form = document.forms['addTask'];
+  const inputTitle = form.elements['title'];
+  const inputBody = form.elements['body'];
+
+
+  // Events
+  renderAllTasks(objOfTask);
+  form.addEventListener('submit', onFormSubmitHandler);
+
+  function renderAllTasks(taskList) {
+    if (!taskList) {
+      console.error('Передайте список задач!');
+      return;
+    }
+
+    const fragment = document.createDocumentFragment();
+
+    Object.values(taskList).forEach(task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+    listContainer.appendChild(fragment);
+  }
+
+
+  function listItemTemplate({
+    _id,
+    title,
+    body
+  } = {}) {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = "bold";
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Deleted task";
+    deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
+
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    return li;
+  }
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+    const titleValue = inputTitle.value;
+    const bodyValue = inputBody.value;
+
+    if (!titleValue || !bodyValue) {
+      alert('Веедіть title i body');
+      return;
+    }
+
+    const task = createNewTask(titleValue, bodyValue);
+    const listItem = listItemTemplate(task);
+    listContainer.insertAdjacentElement('afterbegin', listItem);
+    form.reset();
+  }
+
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`,
+    };
+
+    objOfTask[newTask._id] = newTask;
+    return { ...newTask };
+  }
+
+})(tasks);
